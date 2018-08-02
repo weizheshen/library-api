df14221feca2598f3b0598d2812a8c77fdb73f8d
shenweizhe <weizhe.shen@riking.net>
2018-08-02 12:00:58 +0800

    完善了审核不通过功能及审核不通过后再次申请功能

--- src/main/java/net/riking/libraryapi/controller/borrowbook.java
+++ src/main/java/net/riking/libraryapi/controller/borrowbook.java
@@ -78,5 +78,44 @@ public class borrowbook {
 		return i; //返回操作反馈
 	}
 	
+	
+	
+	@RequestMapping("/appbookagain")
+	public static Integer appbookagain(@RequestParam Integer id) {
+		int i=0;
+		SessionFactory factory = new Configuration().configure().buildSessionFactory();//加载配置
+		Session session=factory.openSession();   //创建会话
+		Transaction tx=session.beginTransaction();
+		try {
+			Query query = session.createQuery("from Record where id = ?");
+			query.setParameter(0, id);
+			Record record = (Record) query.uniqueResult();
+			String code = record.getBookcode();
+			Query query1 = session.createQuery("from Book where code = ?");
+			query1.setParameter(0, code);
+			Book book = (Book) query1.uniqueResult();
+			Integer stock = book.getStock();
+			if(stock != 0) {
+				record.setFlag("审核中");
+				stock--;
+				book.setStock(stock);
+				session.save(record);
+				session.save(book);
+				i=1;
+				
+			}
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			session.getTransaction().rollback();//回滚事务
+		}
+		
+		session.getTransaction().commit();//提交事务
+		session.close();
+		factory.close();
+		
+		return i;
+	}
+	
 
 }
