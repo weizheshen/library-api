ac482a1fb189a08ec06d4eda70fba5747b4fe3c1
shenweizhe <weizhe.shen@riking.net>
2018-12-05 12:27:58 +0800

    IDEA test

---
+++ src/test/java/net/riking/libraryapi/test.java
@@ -0,0 +1,11 @@
+package net.riking.libraryapi;
+
+import lombok.*;
+
+@Data
+public class test {
+    String a;
+    String b;
+    String c;
+
+}
---
+++ src/test/java/net/riking/libraryapi/test2.java
@@ -0,0 +1,10 @@
+package net.riking.libraryapi;
+
+public class test2 {
+    public static void main(String[] args) {
+        test t = new test();
+        t.setA("hello world!");
+        System.out.println(t.getA());
+
+    }
+}
